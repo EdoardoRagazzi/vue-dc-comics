d1ff37b8c4f385d7c70e27076864fb0e75200187
EdoardoRagazzi <edo.ragazzi@gmail.com>
2021-07-07 15:44:06 +0200

    Footer Nav

--- src/components/Footernav.vue
+++ src/components/Footernav.vue
@@ -1,5 +1,17 @@
 <template>
-  <div class="footernav"></div>
+  <div class="footernav">
+    <div class="btn">
+      <button>Sign-up Now</button>
+    </div>
+    <div class="link">
+      <h4>Follow Us</h4>
+      <img src="@/assets/footer-facebook.png" alt="" />
+      <img src="@/assets/footer-pinterest.png" alt="" />
+      <img src="@/assets/footer-twitter.png" alt="" />
+      <img src="@/assets/footer-youtube.png" alt="" />
+      <img src="@/assets/footer-periscope.png" alt="" />
+    </div>
+  </div>
 </template>
 
 <script>
@@ -10,9 +22,50 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss" >
+@import "../style/commons.scss";
+@import "../style/vars.scss";
 .footernav {
+  display: flex;
+  text-transform: $up;
+  justify-content: space-around;
   height: 100px;
   width: 100%;
   background-color: #303030;
+  .btn {
+    display: flex;
+    align-self: center;
+    button {
+      height: 50px;
+      width: 150px;
+      font-size: 1em;
+      font-weight: 600;
+      color: white;
+      text-transform: $up;
+      border: 2px solid #0c7cec;
+      background-color: #303030;
+      &:hover {
+        background-color: #0c7cec;
+        cursor: pointer;
+      }
+    }
+  }
+  .link {
+    line-height: 100px;
+    h4 {
+      font-weight: 900;
+      font-size: 20px;
+      display: inline-block;
+      vertical-align: middle;
+      padding-right: 50px;
+      padding-bottom: 30px;
+      color: #0c7cec;
+    }
+    img {
+      padding-left: 15px;
+      &:hover {
+        cursor: pointer;
+      }
+    }
+  }
 }
 </style>
